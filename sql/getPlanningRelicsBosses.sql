757e40dc18650c02580ddba478ce1cc539c4248c
umeshu909 <oabbeloos@gmail.com>
2025-04-14 13:46:52 +0200

    Correction planning bosses

--- sql/getPlanningRelicsBosses.sql
+++ sql/getPlanningRelicsBosses.sql
@@ -1,3 +1,3 @@
 SELECT id, costpower, rebirthtime, unlocktime
 FROM RuinsNpcAttributeConfig
-WHERE unlocktime IS NOT NULL AND unlocktime <> ''
+WHERE unlocktime IS NOT NULL AND unlocktime <> '' AND unlocktime <> '0'
