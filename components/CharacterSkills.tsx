8038cfe8dd0d3773bacaec13c252bab63ef0a32a
umeshu909 <oabbeloos@gmail.com>
2025-04-15 09:26:03 +0200

    2025-04-15-Mise a jour page des skills
    Suppression traitement en doublon

--- components/CharacterSkills.tsx
+++ components/CharacterSkills.tsx
@@ -170,22 +170,24 @@ async function extractSkills(data: any, lang: string): Promise<Skill[]> {
       const levelKey = `Skill${i}Level${j}`;
       const rawDescription = data[levelKey];
       if (rawDescription) {
-        const res = await fetch("/api/skills/parse", {
+        /*const res = await fetch("/api/skills/parse", {
           method: "POST",
           body: JSON.stringify({ text: rawDescription, dbChoice: lang }),
           headers: {
             "Content-Type": "application/json"
           },
-        });
+        });*/
 
-        const { result } = await res.json();
+        /*const { result } = await res.json();
 
         const withLineBreaks = result
           .replace(/\\n/g, '<br/>')
           .replace(/(^|<br\/?>)(\s*[A-Za-zÀ-ÿ\-]{4,15})\s*:/g, '$1<span class="text-purple-200">$2:</span>')
           .replace(/\[(.*?)\]/g, '<span class="text-orange-300">$1</span>');
 
-        levels.push({ level: j, description: withLineBreaks });
+        levels.push({ level: j, description: withLineBreaks });*/
+        levels.push({ level: j, description: rawDescription });
+
       }
     }
 
--- components/Description.tsx
+++ components/Description.tsx
@@ -27,6 +27,8 @@ const Description = ({ text, dbChoice = "FR" }: Props) => {
         parsedText = parsedText
           .replace(/(\d+([.,]\d+)?[\s\u00A0]*%)/g, '<span style="color: lightgreen;">$1</span>')
           .replace(/(\d+(?:[.,]\d+)?)(?=\s*(secondes?|seconde|sec|couches?|points?|fois?)\b|s\b)/gi, '<span style="color: #82B0D6;">$1</span>')
+          .replace(/\\n/g, '<br/>')
+          .replace(/(^|<br\/?>)(\s*[A-Za-zÀ-ÿ\-]{4,15})\s*:/g, '$1<span class="text-purple-200">$2:</span>')
           .replace(/\[(.*?)\]/g, '<span style="color: orange;">[$1]</span>');
 
         setParsed(parsedText);
