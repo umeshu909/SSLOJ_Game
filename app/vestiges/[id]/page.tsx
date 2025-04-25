cd787cda0d35bd182c3a3f0e10182dc88f71aec1
umeshu909 <oabbeloos@gmail.com>
2025-04-25 13:15:24 +0200

    2025-04-25-Correction page des Vestiges
    Ajout des stats début, fin, delais et cooldown

--- app/vestiges/[id]/page.tsx
+++ app/vestiges/[id]/page.tsx
@@ -8,16 +8,21 @@ import "swiper/css";
 import Description from "@/components/Description";
 
 interface VestigeDetail {
-    id: number;
-    name: string;
-    pic: string;
-    skillid: string;
-    skillname: string;
-    level: number;
-    icon: string;
-    desc: string;
-    quality: number;
+  id: number;
+  name: string;
+  pic: string;
+  skillid: string;
+  skillname: string;
+  level: number;
+  icon: string;
+  desc: string;
+  quality: number;
+  starttm: number;
+  skillendtm: number;
+  precd: number;
+  cd: number;
 }
+
 
 interface CareerValue {
     metier: number;
@@ -260,6 +265,31 @@ const VestigeDetailPage = () => {
                                                         );
                                                     })}
                                                 </div>
+
+                                                <div className="text-sm font-semibold text-white flex flex-wrap gap-x-4 gap-y-1 border-t border-white/10 p-4">
+                                                    {skill.starttm !== undefined && skill.starttm !== 0 && (
+                                                        <span>
+                                                            <span className="font-normal text-white/80">Début:</span> {(skill.starttm / 1000).toFixed(2)}s
+                                                        </span>
+                                                    )}
+                                                    {skill.skillendtm !== undefined && skill.skillendtm !== 0 && (
+                                                        <span>
+                                                            <span className="font-normal text-white/80">Fin:</span> {(skill.skillendtm / 1000).toFixed(2)}s
+                                                        </span>
+                                                    )}
+                                                    {skill.precd !== undefined && skill.precd !== 0 && (
+                                                        <span>
+                                                            <span className="font-normal text-white/80">Délai:</span> {(skill.precd / 1000).toFixed(2)}s
+                                                        </span>
+                                                    )}
+                                                    {skill.cd !== undefined && skill.cd !== 0 && (
+                                                        <span>
+                                                            <span className="font-normal text-white/80">Cooldown:</span> {(skill.cd / 1000).toFixed(2)}s
+                                                        </span>
+                                                    )}
+                                                </div>
+
+
                                             </div>
                                         </div>
                                     </div>
--- sql/getVestigesDetail.sql
+++ sql/getVestigesDetail.sql
@@ -2,12 +2,16 @@ SELECT DISTINCT HolyMarkBaseConfig.id, HolyMarkBaseConfig.name, HolyMarkBaseConf
 SkillTextConfig.skillid,
 SkillTextConfig.name AS skillname,
 SkillTextConfig.level,
+SkillConfig.starttm,
+SkillConfig.skillendtm,
+SkillConfig.precd,
+SkillConfig.cd,
 SkillConfig.icon,
 SkillTextConfig.desc 
 FROM HolyMarkBaseConfig
 INNER JOIN SkillTextConfig ON ( SkillTextConfig.skillid = HolyMarkBaseConfig.skill1 
                              OR SkillTextConfig.skillid = HolyMarkBaseConfig.skill2 
                              OR SkillTextConfig.skillid = HolyMarkBaseConfig.skill3 )
-INNER JOIN SkillConfig ON SkillConfig.skillid = SkillTextConfig.skillid
+INNER JOIN SkillConfig ON SkillConfig.skillid = SkillTextConfig.skillid AND SkillConfig.level = SkillTextConfig.level
 WHERE HolyMarkBaseConfig.id = ?
 AND SkillTextConfig.desc != '' ;
