9df1e288beb7208ebbf1eef8b63156640566e74b
umeshu909 <oabbeloos@gmail.com>
2025-04-25 11:16:19 +0200

    2025-04-25-Correction des plannings

--- sql/getPlanningRelics.sql
+++ sql/getPlanningRelics.sql
@@ -1,10 +1,10 @@
 SELECT 
     opentime AS start,
     enrollday AS enrollday,
-    quietday AS stamina_days,
-    (stopday - quietday) AS silence_days,
+    (quietday - 1) AS stamina_days,
+    (stopday - quietday + 1) AS silence_days,
     totaldays AS total_days,
     peoplenum AS players
 FROM RuinsMapConfig
-WHERE opentime >= datetime('now', '-14 days')
+WHERE opentime >= datetime('now', '-40 days')
 ORDER BY opentime ASC;
