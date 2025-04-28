05434b8e24cae1941a0dda9ac69b133950731075
umeshu909 <oabbeloos@gmail.com>
2025-04-29 01:59:48 +0200

    modif page

--- pages/api/articles/latest.ts
+++ pages/api/articles/latest.ts
@@ -7,7 +7,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     const response = await fetch(`${CMS_URL}/items/Articles?fields=id,title,text,date_created,images&sort=-date_created&limit=3`)
     const data = await response.json()
-    res.status(200).json(data.data) // Attention : Directus retourne "data" et pas "docs"
+    res.status(200).json({ data: data.data })
+
   } catch (err) {
     console.error('Erreur fetch articles :', err)
     res.status(500).json({ error: 'Impossible de charger les articles' })
