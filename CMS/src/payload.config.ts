012649a5d5b5abe7a1d63f3f5147b53f71628b0a
umeshu909 <oabbeloos@gmail.com>
2025-04-28 11:18:13 +0200

    Correction Payload 5

--- CMS/src/payload.config.ts
+++ CMS/src/payload.config.ts
@@ -19,7 +19,7 @@ const dirname = path.dirname(filename)
 export default buildConfig({
   admin: {
     user: Users.slug,
-    path: '/admin'
+    path: '/admin',
     importMap: {
       baseDir: path.resolve(dirname),
     },
