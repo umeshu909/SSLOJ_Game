f26164b7ad2dd02ebce5301a34520db3c26d4a99
umeshu909 <oabbeloos@gmail.com>
2025-04-28 12:45:11 +0200

    payload correction 12

--- CMS/src/app/cms/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function CMSLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
--- CMS/src/app/cms/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getPayloadClient } from '@/payload/payloadClient';
-
-export default async function CMSPage() {
-  const payload = await getPayloadClient();
-
-  return payload.admin.render();
-}
