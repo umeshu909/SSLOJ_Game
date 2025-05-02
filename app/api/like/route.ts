5f27907951ea26c8eb5ddd69dae6641712e41f6a
umeshu909 <oabbeloos@gmail.com>
2025-05-02 10:55:14 +0200

    mise a jour route likes

--- app/api/like/route.ts
+++ app/api/like/route.ts
@@ -1,8 +1,9 @@
 // app/api/like/route.ts
 export async function POST(req: Request) {
   const { id, likes } = await req.json();
+  const API_URL = process.env.PUBLIC_INTERNAL_API_URL || 'http://localhost:8055';
 
-  const res = await fetch(`http://localhost:8055/items/Articles/${id}`, {
+  const res = await fetch(`${API_URL}/items/Articles/${id}`, {
     method: 'PATCH',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ likes }),
