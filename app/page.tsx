6cd292425230f9528aa9690f710293f6a51040c6
umeshu909 <oabbeloos@gmail.com>
2025-05-13 09:29:39 +0200

    2025-05-13-Ajout preview Appolon

--- app/page.tsx
+++ app/page.tsx
@@ -130,11 +130,11 @@ export default function Home() {
 
                   <div className="relative w-40 h-fit">
                     <img
-                      src="/images/actual/K_panduola_nd.png"
+                      src="/images/actual/K_aboluo_taiyangshen.png"
                       className="rounded-lg w-full h-auto"
                     />
                     <div className="absolute bottom-0 left-0 right-0 bg-black/60 text-yellow-400 text-sm font-semibold text-center py-1 rounded-b-lg z-10">
-                      Pandore ND
+                      Appolon
                     </div>
                   </div>
 
